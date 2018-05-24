59305f9526f46ad07513b8a7c1dc7c0eb520350b
marthabanana <marthag@afrobananarepublic.com>
2018-05-24 13:41:49 +0300

    Update fixtures.js

--- src/timetable/fixtures.js
+++ src/timetable/fixtures.js
@@ -28,9 +28,117 @@ export default [
     date: new Date('07/22/2018 18:00'),
     stage: STAGE3,
   },
+
+  THE ARC FRIDAY
+  {
+    id: 'B-MAN',
+    date: new Date('07/20/2018 23:00'),
+    stage: STAGE1,
+  },
    {
-    id: 'ED',
+    id: 'ABR RADIO',
+    date: new Date('07/20/2018 23:00'),
+    stage: STAGE1,
+  },
+    {
+    id: 'ABR RADIO',
+    date: new Date('07/20/2018 14:00'),
+    stage: STAGE1,
+  },
+    {
+    id: 'SPIVAK',
+    date: new Date('07/20/2018 16:00'),
+    stage: STAGE1,
+  },
+    {
+    id: 'APARAPIRA PARAPE',
+    date: new Date('07/20/2018 18:00'),
+    stage: STAGE1,
+  },
+    {
+    id: 'BOMBASOUL',
+    date: new Date('07/20/2018 20:00'),
+    stage: STAGE1,
+  },
+    {
+    id: 'CHEVY',
+    date: new Date('07/20/2018 22:00'),
+    stage: STAGE1,
+  },
+    {
+    id: 'AFROFOX',
+    date: new Date('07/21/2018 00:00'),
+    stage: STAGE1,
+  },
+    {
+    id: 'MOR ELIAN',
     date: new Date('07/21/2018 02:00'),
     stage: STAGE1,
   },
+  
+  THE ARC SATURDAY
+    {
+    id: 'CLAUDIO',
+    date: new Date('07/21/2018 09:30'),
+    stage: STAGE1,
+  },
+    {
+    id: 'ABR RADIO',
+    date: new Date('07/20/2018 23:00'),
+    stage: STAGE1,
+  },
+    {
+    id: 'MESCALIDO SOUNDS',
+    date: new Date('07/21/2018 11:30'),
+    stage: STAGE1,
+  },
+    {
+    id: 'RUMBA ATTACK',
+    date: new Date('07/21/2018 13:30'),
+    stage: STAGE1,
+  },
+    {
+    id: 'SOFRONIS',
+    date: new Date('07/21/2018 15:00'),
+    stage: STAGE1,
+  },
+     {
+    id: 'MAGOS & CHARIS',
+    date: new Date('07/21/2018 17:00'),
+    stage: STAGE1,
+  },
+     {
+    id: 'THE DUDE OF STRATOSPHEAR',
+    date: new Date('07/21/2018 19:00'),
+    stage: STAGE1,
+  },
+     {
+    id: 'SULTAN SHAKES',
+    date: new Date('07/21/2018 20:00'),
+    stage: STAGE1,
+  },
+     {
+    id: 'SIXONESIX',
+    date: new Date('07/21/2018 21:00'),
+    stage: STAGE1,
+  },
+     {
+    id: 'GEORGE D'ANDRE',
+    date: new Date('07/21/2018 15:00'),
+    stage: STAGE1,
+  },
+       {
+    id: 'ALEXIS',
+    date: new Date('07/22/2018 00:00'),
+    stage: STAGE1,
+  },
+       {
+    id: 'ED DAVENPORT',
+    date: new Date('07/22/2018 02:00'),
+    stage: STAGE1,
+  },
+  
+  
+
+
 ]
