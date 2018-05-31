26c4528032042062faec24829a3e307207b9d036
marthabanana <marthag@afrobananarepublic.com>
2018-05-31 15:41:52 +0300

    Create z-elenitsa-workshop.js

---
+++ src/artists/fixtures/z-elenitsa-workshop.js
@@ -0,0 +1,19 @@
+import image from '../images/sample-artis-01.jpg'
+
+export const url = ''
+export const photo = image
+
+// Markdown fields
+export const name = 'ELENITSA WORKSHOP'
+export const genres = ''
+export const text = `
+---
+`.trim()
+
+export default {
+  name,
+  genres,
+  url,
+  photo,
+  text,
+}
