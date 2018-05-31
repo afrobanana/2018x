b0185ed3a9fe9b99d2f162d91f078e2e5c8f8664
marthabanana <marthag@afrobananarepublic.com>
2018-05-31 15:06:26 +0300

    Update GEORGE-DANDRE.js

--- src/artists/fixtures/GEORGE-DANDRE.js
+++ src/artists/fixtures/GEORGE-DANDRE.js
@@ -4,7 +4,7 @@ export const url = 'https://soundcloud.com/georges-dandre'
 export const photo = image
 
 // Markdown fields
-export const name = 'GEORGE D'ANDRE *(CY)*'
+export const name = 'GEORGE D\'ANDRE *(CY)*'
 export const genres = '*DEEP* *ACID* *FUNKY HOUSE* *DISCO* *AFROBEAT* *TECHNO* *SOUL*'
 export const text = `
 George is one cool DJ and his unparalleled love and passion for dance music have seen him delight crowds in clubs in bars, gigs and festivals around Cyprus with deep and funky house. But he’s not constrained by genre boundaries and always mixes things up with a touch of disco, afrobeat, techno, funk and soul to lift the spirits. Georges D’ André is a founding member of the Whizzy Wigs crew, with whom he organizes gigs with likeminded DJs and artists from around the world, while also a member of the Honest Electronics collective of artists and DJs.
