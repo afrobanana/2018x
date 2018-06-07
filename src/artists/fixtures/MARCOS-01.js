7253b6ad09679f970a07fce590e183e7fd68d427
marthabanana <marthag@afrobananarepublic.com>
2018-06-07 17:39:15 +0300

    Update MARCOS-01.js

--- src/artists/fixtures/MARCOS-01.js
+++ src/artists/fixtures/MARCOS-01.js
@@ -4,10 +4,10 @@ export const url = 'https://www.facebook.com/christodoulos.hadji '
 export const photo = image
 
 // Markdown fields
-export const name = 'ANCHORSONG *(JP)* *(LIVE)*'
-export const genres = '*DISCO* *HOUSE* *TECHNO*'
+export const name = 'MARCOS *(CY)*'
+export const genres = '*DISCO* *HOUSE* *AFROBEAT*'
 export const text = `
-With a devotion to electronic music and broad music knowledge in general, Marcos’s selection of disco and house blended with Balearic and Afro-beat sounds, will set the mood for an adventurous musical journey. A newcomer at ABR Festival, he is usually seen playing groovy DJ sets at numerous venues and parties across the island.
+With  a  devotion  to  electronic  music  and  broad  music  knowledge  in  general,  Marcos’s  selection  of  disco  and  house  blended  with  Balearic  and  Afro-beat  sounds,  will  set  the  mood  for  an  adventurous  musical  journey.  A  newcomer  at  ABR  Festival, he  is  usually  seen  playing  groovy  dj  sets  at  numerous  venues  and  parties  across  the  island.
 `.trim()
 
 export default {
