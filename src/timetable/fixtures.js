b8ed63fd7640d3a69baf4d3c76a80b7dd6e83622
marthabanana <marthag@afrobananarepublic.com>
2018-06-07 18:00:47 +0300

    Update fixtures.js

--- src/timetable/fixtures.js
+++ src/timetable/fixtures.js
@@ -1,22 +1,27 @@
 export const STAGE1 = {
-  name: 'THE ARC',
+  name: 'Daytime at THE ARC',
   type:
   displayOrder: 0,
 }
 export const STAGE2 = {
-  name: 'THE WORLD',
+  name: 'Night Time at THE ARC',
   type:
   displayOrder: 1,
 }
 export const STAGE3 = {
-  name: 'THE BEACH',
+  name: 'THE WORLD',
   type:
   displayOrder: 2,
 }
 export const STAGE4 = {
+  name: 'THE BEACH',
+  type:
+  displayOrder: 3,
+}
+export const STAGE4 = {
   name: 'KIDS INNOVATION',
   type: 'activity',
-  displayOrder: 2,
+  displayOrder: 4,
 }
 
 export default [
@@ -32,29 +37,29 @@ export default [
     stage: STAGE1,
   },
     {
-    id: 'APARAPIRA_PARAPE',
+    id: 'APARAPIRA-PARAPE',
     date: new Date('07/20/2018 18:00'),
     stage: STAGE1,
   },
     {
     id: 'BOMBASOUL',
     date: new Date('07/20/2018 20:00'),
-    stage: STAGE1,
+    stage: STAGE2,
   },
     {
     id: 'CHEVY',
     date: new Date('07/20/2018 22:00'),
-    stage: STAGE1,
+    stage: STAGE2,
   },
     {
     id: 'AFROFOX',
     date: new Date('07/21/2018 00:00'),
-    stage: STAGE1,
+    stage: STAGE2,
   },
     {
     id: 'MOR-ELIAN',
     date: new Date('07/21/2018 02:00'),
-    stage: STAGE1,
+    stage: STAGE2,
   },
 
   //THE ARC SATURDAY
@@ -79,116 +84,116 @@ export default [
     stage: STAGE1,
   },
      {
-    id: 'MAGOS & CHARIS',
+    id: 'MAGOS-CHARIS',
     date: new Date('07/21/2018 17:00'),
     stage: STAGE1,
   },
      {
-    id: 'DUDE-OF-STRATOSPHEAR',
+    id: 'DUDE-OF-STRATOSPHEAR-1',
     date: new Date('07/21/2018 19:00'),
     stage: STAGE1,
   },
      {
     id: 'SULTAN-SHAKES',
     date: new Date('07/21/2018 20:00'),
-    stage: STAGE1,
+    stage: STAGE2,
   },
      {
-    id: 'SIXONESIX',
+    id: 'SIXONESIX-01',
     date: new Date('07/21/2018 21:00'),
-    stage: STAGE1,
+    stage: STAGE2,
   },
      {
     id: 'GEORGE-DANDRE',
     date: new Date('07/21/2018 15:00'),
-    stage: STAGE1,
+    stage: STAGE2,
   },
      {
     id: 'ALEXIS',
     date: new Date('07/22/2018 00:00'),
-    stage: STAGE1,
+    stage: STAGE2,
   },
        {
     id: 'ED',
     date: new Date('07/22/2018 02:00'),
-    stage: STAGE1,
+    stage: STAGE2,
   },
 
   //MAIN STAGE FRIDAY
 
       {
-    id: 'MARCOS-OTHER-STAGE.js',
+    id: 'MARCOS-01',
     date: new Date('07/20/2018 19:30'),
-    stage: STAGE2,
+    stage: STAGE3,
   },
     {
-    id: 'MIGHT-SUGARCANE01',
+    id: 'MIGHTY-SUGARCANE',
     date: new Date('07/20/2018 20:30'),
-    stage: STAGE2,
+    stage: STAGE3,
   },
     {
     id: 'AFRODYSSEY-ORCHESTRA',
     date: new Date('07/20/2018 22:00'),
-    stage: STAGE2,
+    stage: STAGE3,
   },
       {
     id: 'PENYA',
     date: new Date('07/20/2018 23:30'),
-    stage: STAGE2,
+    stage: STAGE3,
   },
       {
     id: 'RICOLOOP',
     date: new Date('07/21/2018 01:00'),
-    stage: STAGE2,
+    stage: STAGE3,
   },
 
   //MAIN STAGE SATURDAY
   {
     id: 'MACUMBA',
     date: new Date('07/21/2018 20:30'),
-    stage: STAGE2,
+    stage: STAGE3,
   },
       {
     id: 'EBO-TAYLOR',
     date: new Date('07/21/2018 22:00'),
-    stage: STAGE2,
+    stage: STAGE3,
   },
       {
-    id: 'MINYATA',
+    id: 'MINYANTA',
     date: new Date('07/21/2018 23:30'),
-    stage: STAGE2,
+    stage: STAGE3,
   },
       {
     id: 'MAD-MAD-MAD',
     date: new Date('07/22/2018 01:00'),
-    stage: STAGE2,
+    stage: STAGE3,
   },
 
   //BEACH STAGE
       {
-    id: 'MARCOS-BEACH',
+    id: 'MARCOS-02',
     date: new Date('07/22/2018 10:00'),
-    stage: STAGE3,
+    stage: STAGE4,
   },
       {
-    id: 'DUDE',
+    id: 'DUDE-OF-STRATOSPHEAR-2',
     date: new Date('07/22/2018 12:00'),
-    stage: STAGE3,
+    stage: STAGE4,
   },
       {
-    id: 'SIXONESIX-BEACH',
+    id: 'SIXONESIX-2',
     date: new Date('07/22/2018 14:00'),
-    stage: STAGE3,
+    stage: STAGE4,
   },
       {
     id: 'PAUL-LAZA',
     date: new Date('07/22/2018 16:00'),
-    stage: STAGE3,
+    stage: STAGE4,
   },
       {
-    id: 'COTSIOS-BEACH',
+    id: 'COTSIOS-O-PIKATILLIS',
     date: new Date('07/22/2018 18:00'),
-    stage: STAGE3,
+    stage: STAGE4,
   },
 
 
