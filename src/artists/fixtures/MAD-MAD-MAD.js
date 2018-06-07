049048c551534b89b7e8aca7fa07a40f23c65d51
marthabanana <marthag@afrobananarepublic.com>
2018-06-07 17:20:17 +0300

    Update MAD-MAD-MAD.js

--- src/artists/fixtures/MAD-MAD-MAD.js
+++ src/artists/fixtures/MAD-MAD-MAD.js
@@ -1,10 +1,10 @@
-import image from '../images/madmadmad-01.jpg'
+import image from '../images/MADMADMAD.jpg'
 
 export const url = 'http://bit.ly/MADMADMAD'
 export const photo = image
 
 // Markdown fields
-export const name = 'MAD MAD MAD *(UK)* *(LIVE)*'
+export const name = 'MADMADMAD *(UK)* *(LIVE)*'
 export const genres = '*DISCO*'
 export const text = `
 Ready for Mad Mad Mad? This unique band’s music can best be described as ‘bonkers electro meets techno stomper’. An electro, krauf, funk outfit, they’ve been influenced by Liquid Liquid, Black Devil Disco Club and the Residents. And they’ll be causing a storm at the festival as they perform a live bass and drum rhythm section with a station of electronics. We. Can’. Wait!
