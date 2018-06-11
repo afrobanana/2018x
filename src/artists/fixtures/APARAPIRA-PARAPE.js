631077013edd02077de7a655aa005322bd179f31
marthabanana <marthag@afrobananarepublic.com>
2018-06-11 17:32:55 +0300

    Update APARAPIRA-PARAPE.js

--- src/artists/fixtures/APARAPIRA-PARAPE.js
+++ src/artists/fixtures/APARAPIRA-PARAPE.js
@@ -4,7 +4,7 @@ export const url = 'https://www.facebook.com/AparapiraParapeMusic/'
 export const photo = image
 
 // Markdown fields
-export const name = 'APARAPIRA PARAPE *(CY)*'
+export const name = 'APARAPIRA PARAPE *(DJ SET)*'
 export const genres = '*HOUSE* *SOUL* *DISCO* *ELECTRONIC*'
 export const text = `
 From wonky acid to disco glory, this duo are known for their string of successful Limassol parties, and have rapidly established a name for themselves as dynamic selectors who work up a real frenzy wherever they go. For now, they just can’t wait to work their groove on with revelers at the ABR Festival with their party starting sets!
