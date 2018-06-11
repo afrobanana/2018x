22afd5bb57c03eefe8127921057cb2b63db7169c
marthabanana <marthag@afrobananarepublic.com>
2018-06-11 17:33:59 +0300

    Update CLAUDIO.js

--- src/artists/fixtures/CLAUDIO.js
+++ src/artists/fixtures/CLAUDIO.js
@@ -4,7 +4,7 @@ export const url = 'www.mixcloud.com/cloud10music'
 export const photo = image
 
 // Markdown fields
-export const name = 'CLAUDIO *(CY)*'
+export const name = 'CLAUDIO *(DJ SET)*'
 export const genres = '*DISCO* *JAZZ* *BLUES* *FUNK FUSION*'
 export const text = `
 There aren’t many local bar hoppers that don’t know DJ Claudio a regular selector at some of Cyprus’ coolest venues and parties and co-organizer of the famed Nostalgia Parties. Others recognize him for his ongoing work in the live music scene, while his eclectic taste spans everything from blues, rock, jazz and soul, to disco, electro, hip hop and DnB. This year’s performance at ABR Festival will be his third a soothing vinyl-only set fusing Afro, jazz, blues and funk.
