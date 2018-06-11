95975fcfc944c1123405f9db1fdf2f5197df76f8
marthabanana <marthag@afrobananarepublic.com>
2018-06-11 17:39:59 +0300

    Update SULTAN-SHAKES.js

--- src/artists/fixtures/SULTAN-SHAKES.js
+++ src/artists/fixtures/SULTAN-SHAKES.js
@@ -4,7 +4,7 @@ export const url = 'http://bit.ly/SULTANSHAKES'
 export const photo = image
 
 // Markdown fields
-export const name = 'SULTAN SHAKES *(UK)* '
+export const name = 'SULTAN SHAKES *(UK)* *(DJ SET)*'
 export const genres = '*COSMIC* *BREAKS* *TECHNO*'
 export const text = `
 Ready for the ultimate ‘shake’ experience? This DJ is a resident at one of Hackney’s hippest venues - The Glove that Fits. And he’ll be taking time out to play some sparkling house and disco that’s bound to leave you begging for more!
