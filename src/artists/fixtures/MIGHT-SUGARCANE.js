98b077c3fd7310bbebd85a77a391d1a5b9cf381d
marthabanana <marthag@afrobananarepublic.com>
2018-06-11 17:37:23 +0300

    Update MIGHT-SUGARCANE.js

--- src/artists/fixtures/MIGHT-SUGARCANE.js
+++ src/artists/fixtures/MIGHT-SUGARCANE.js
@@ -4,7 +4,7 @@ export const url = 'http://bit.ly/MIGHTYSUGAR'
 export const photo = image
 
 // Markdown fields
-export const name = 'MIGHTY SUGARCANE *(CY)* *(LIVE)*'
+export const name = 'MIGHTY SUGARCANE *(LIVE)*'
 export const genres = '*GROOVE* *FUNK* *ELECTRONIC*'
 export const text = `
 A super funky electronic groove- based group consisting of Marios Takoushis on keyboards, Cahit Kutrafali on electric bass and Stelios Xydias on drums, the mighty fine trio will be adding their exceptional spark to the ABR Festival with great jazz influences and a mix of their own uplifting compositions.
