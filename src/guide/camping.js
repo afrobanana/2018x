2d2dda870acae6dd1da0bb4ec42a27cc2cef0d99
marthabanana <marthag@afrobananarepublic.com>
2018-06-18 19:21:36 +0300

    Update camping.js

--- src/guide/camping.js
+++ src/guide/camping.js
@@ -6,44 +6,20 @@ export default () => (
   <section className="guide-camping">
     <img src={campingImg} alt="Camping is great!" />
     <p>
-      The camping area is divided into different zones to best suit your needs,
-      with an allocated ‘silent zone’ for those who’d just like to sleep and
-      relax. There’s also limited space for caravans to set up base and an
-      accessible area reserved for disabled campers. For your convenience, there
-      are designated areas to pump up your mattress and a charging station – but
-      please bring your own chargers/pumps!
+      To make your festival camping experience as pleasant and comfortable as possible, ample shower and toilet facilities are available at the site. We also recommend you bring those trusty little tent pegs to pin your tents down, as it can get quite windy. For your convenience, there is a charging station and designated areas to pump inflatable mattresses – please bring your own chargers/pumps! Camping is only allowed in the allocated area within the festival site and unfortunately, finding somewhere to string up a hammock is unlikely, so we suggest you leave it at home.
     </p>
     <dl>
       <dt>CAMPSITE PASS</dt>
       <dd>
-        In order to camp on the festival camping grounds, an admission pass is
-        required, with an additional tent or caravan pass that must be purchased
-        separately. Camping passes are limited, so be sure to get hold of yours
-        in due time. Only one camping pass per tent or caravan is required.
+        If festival camping in the great outdoors is your thing, you’ll first need an admission pass, and then an additional tent pass that is purchased separately. Camping passes are also limited, so get hold of yours in time to avoid disappointment. Only one camping pass per tent is required.
       </dd>
-
-      <dt>CAMPERS' DROP-OFF POINT</dt>
-      <dd>
-        For your convenience, there is an allocated campers' drop-off point near
-        the festival entrance where you can drop-off your friends and camping
-        gear before proceeding to the parking area. Please do not leave any of
-        your belongings unattended – the festival is not responsible for any
-        losses, theft or damages.
-      </dd>
-
-      <dt>CAMPERS' PARKING</dt>
-      <dd>
-        A small parking area just a few minutes’ walk from the festival site has
-        been reserved for campers which will operate on a first-come
-        first-served basis. The parking spot can only be accessed with a valid
-        3-DAY PASS with the additional 2-NIGHT TENT PASS.
-      </dd>
-
+  
       <dt>PLEASE REMEMBER</dt>
       <dd>
         <ul>
           <li>Turn off taps and showers when finished.</li>
           <li>Don't leave rubbish or your camping gear behind!</li>
+          <li>Smokers, grab a reusable ashtray on the way in, and use it to store your cigarette butts instead of chucking them on the ground..</li>
           <li>Re-use your carrier bags.</li>
           <li>
             Take recycling and rubbish bags with you and keep the campsite clean
