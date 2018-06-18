68522608ddd4aa9d8ca2fc51a5c7928ed681d0e7
marthabanana <marthag@afrobananarepublic.com>
2018-06-18 22:05:06 +0300

    Update PARTNERYOGA.js

--- src/activities/fixtures/PARTNERYOGA.js
+++ src/activities/fixtures/PARTNERYOGA.js
@@ -3,7 +3,7 @@ import image from '../images/PARTNERYOGA.jpg'
 export const photo = image
 
 // Markdown fields
-export const name = 'PARTNER YOGA'
+export const name = 'PARTNER YOGA *(KIDS FRIENDLY)*'
 export const text = `
 Partner yoga cultivates a sense of playfulness, connection and fun in your yoga practice. And during this unique session, expert practitioners Nina and Stavri will teach you to tune in and explore connection, support and trust, through grounding partner poses. This workshop will nourish your body and heart, helping you to understand the true meaning of connecting with yourself, your partner and the whole group. Bring your parent, child, sibling, friend or partner and join the movement! 
 
