d272abe726f8ecdc3ac374ef305f4f2708b5bc33
marthabanana <marthag@afrobananarepublic.com>
2018-06-18 18:45:01 +0300

    Update BIGIBANG.js

--- src/activities/fixtures/BIGIBANG.js
+++ src/activities/fixtures/BIGIBANG.js
@@ -3,15 +3,15 @@ import image from '../images/BIGIBANG.jpg'
 export const photo = image
 
 // Markdown fields
-export const name = 'Take flight with AcroYoga Jam'
+export const name = 'BIGIBANG PROJECT'
 export const text = `
-Dissolve your fears and tap into new and infinite possibilities of communication, trust, and union with a unique AcroYoga Jam. Combining acrobatics, yoga, and healing arts, the practice of AcroYoga has built a community of active teachers and practitioners all over the world and the ABR Festival will now witness special guest and Jambassador of AcroYoga International for Cyprus, Skevi Tofaridou Chrysou, sharing her knowledge and passion about healthier and mindful-conscious living with all of you! Prepare for an exciting blend of yoga wisdom, the dynamism of acrobatics, and the loving kindness of healing arts.
+If there is one thing that almost all kids are seriously good at, its making a bucket load of noise. And they’ll get to do just that at this riotously fun workshop aimed at boisterous little monsters with little or no knowledge of music. Drop your precious cargo off for an hour of experimenting with makeshift Junk Percussion instruments; learning new beats and rhythms; expressing themselves freely and creatively and generally letting off steam in a safe environment. You never know, you might have the world’s next John Bonham on your hands!
 
-**Capacity:** No limits
+**Capacity:** 20 participants
 
 **Duration:** 1 hour
 
-**Age Group:**  All ages
+**Age Group:**  Children aged 7 - 12 yrs
 `.trim()
 
 export default {
