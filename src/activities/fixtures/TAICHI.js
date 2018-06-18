e46fe3a5e09c399f41ffb650271a85e2b5bafe2a
marthabanana <marthag@afrobananarepublic.com>
2018-06-18 20:20:00 +0300

    Update TAICHI.js

--- src/activities/fixtures/TAICHI.js
+++ src/activities/fixtures/TAICHI.js
@@ -7,7 +7,7 @@ export const name = 'SUNSET TAI CHI'
 export const text = `
 Join instructor Loizos on the beach at sunset, to balance, relax and calm the body and mind through the healing energy of Tai Chi and Qigong. These widespread Chinese Arts have been practiced for hundreds of years and by millions of people all around the world. And while the advantages have long been obvious to those who dare to try, modern science has now also confirmed the long list of health benefits associated with these unique transcendent arts of the Far East.
 
-**Capacity:** No limits
+**Capacity:** 30 participants
 
 **Duration:** 1 hour
 
