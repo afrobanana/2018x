58bb72407b2802d471e4f69d8f98bba8ac8078bd
marthabanana <marthag@afrobananarepublic.com>
2018-06-18 18:56:40 +0300

    Update PERPATOPERPATO.js

--- src/activities/fixtures/PERPATOPERPATO.js
+++ src/activities/fixtures/PERPATOPERPATO.js
@@ -3,7 +3,7 @@ import image from '../images/PERPATOPERPATO.jpg'
 export const photo = image
 
 // Markdown fields
-export const name = 'ΠΕΡΠΑΤΩ, ΠΕΡΠΑΤΩ ΣΤΟ ΔΑΣΟΣ'
+export const name = 'ΠΕΡΠΑΤΩ, ΠΕΡΠΑΤΩ ΣΤΟ ΔΑΣΟΣ *(PERFORMANCE)*'
 export const text = `
 The surreal magic of a festival is the perfect environment to lose yourself in nature’s enchanting mysticism. At this performance, inquisitive festival goers of all ages will use all five senses to learn, discover and explore; listening to the fairy tales of the forest and uncovering its hidden secrets. What hides under the wings of a golden eagle? What does a giant, a princess, a boatman, a magician and an old lady have to smile about? And who is more cunning than a fox? Grab your parents and find out together!
 
