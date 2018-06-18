deeecfab9888087f0d4b08f4466fa0e695f8b004
marthabanana <marthag@afrobananarepublic.com>
2018-06-18 20:21:02 +0300

    Update CAPOEIRA.js

--- src/activities/fixtures/CAPOEIRA.js
+++ src/activities/fixtures/CAPOEIRA.js
@@ -7,7 +7,7 @@ export const name = 'CAPOEIRA ADULT TASTER CLASS'
 export const text = `
 At this exciting and energising taster class, participants will learn about the unusual history behind this fascinating and uplifting Brazilian martial art, at the same time getting to grips with the unique movements and music. Driven by a desire to bring people together and experience something totally new and different, your instructor is a master at encouraging you to let go, be free, loosen up and leave your inhibitions behind!
 
-**Capacity:** No limits
+**Capacity:** 30 participants
 
 **Duration:** 1 hour
 
