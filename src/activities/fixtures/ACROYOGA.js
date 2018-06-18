cf4a476479a10c51e2e7d459cf903edd1b22a8a9
marthabanana <marthag@afrobananarepublic.com>
2018-06-18 18:32:29 +0300

    Update ACROYOGA.js

--- src/activities/fixtures/ACROYOGA.js
+++ src/activities/fixtures/ACROYOGA.js
@@ -3,7 +3,7 @@ import image from '../images/ACROYOGA.jpg'
 export const photo = image
 
 // Markdown fields
-export const name = 'Take flight with AcroYoga Jam'
+export const name = 'TAKE FLIGHT WITH ACROYOGA JAM'
 export const text = `
 Dissolve your fears and tap into new and infinite possibilities of communication, trust, and union with a unique AcroYoga Jam. Combining acrobatics, yoga, and healing arts, the practice of AcroYoga has built a community of active teachers and practitioners all over the world and the ABR Festival will now witness special guest and Jambassador of AcroYoga International for Cyprus, Skevi Tofaridou Chrysou, sharing her knowledge and passion about healthier and mindful-conscious living with all of you! Prepare for an exciting blend of yoga wisdom, the dynamism of acrobatics, and the loving kindness of healing arts.
 
