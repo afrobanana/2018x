d439b7b77a296400396aa7083e30a0f68ea02b5b
marthabanana <marthag@afrobananarepublic.com>
2018-06-18 20:20:26 +0300

    Update CHROMATAYFES.js

--- src/activities/fixtures/CHROMATAYFES.js
+++ src/activities/fixtures/CHROMATAYFES.js
@@ -6,7 +6,7 @@ export const photo = image
 export const name = 'COLOURS, TEXTURES, FLAVOURS, SMELLS'
 export const text = `What do we eat, and why? How do we eat it, and where does it come from? What do we want from the meals, and what do we need? And what roles do our body, mind, soul, society and economic standing, play in our lives? If these are questions you want to know the answers to, join in for this inspiring and educational food workshop, and experiment with new ingredients such as dry and fresh fruits, vegetables and herbs. Everyone is welcome.
 
-**Capacity:** No limits
+**Capacity:** 60 participants
 
 **Duration:** 1 hour
 
