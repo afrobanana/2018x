6598a2836604495cb7aa9d3f71e498deb7d8b3b4
marthabanana <marthag@afrobananarepublic.com>
2018-06-18 18:51:24 +0300

    Update LIFESTORIES.js

--- src/activities/fixtures/LIFESTORIES.js
+++ src/activities/fixtures/LIFESTORIES.js
@@ -3,15 +3,15 @@ import image from '../images/LIFESTORIES.jpg'
 export const photo = image
 
 // Markdown fields
-export const name = 'Take flight with AcroYoga Jam'
+export const name = 'COMMUNITY MUSIC FOR INCLUSION'
 export const text = `
-Dissolve your fears and tap into new and infinite possibilities of communication, trust, and union with a unique AcroYoga Jam. Combining acrobatics, yoga, and healing arts, the practice of AcroYoga has built a community of active teachers and practitioners all over the world and the ABR Festival will now witness special guest and Jambassador of AcroYoga International for Cyprus, Skevi Tofaridou Chrysou, sharing her knowledge and passion about healthier and mindful-conscious living with all of you! Prepare for an exciting blend of yoga wisdom, the dynamism of acrobatics, and the loving kindness of healing arts.
+Sistema Cyprus is a social project aiming to make music education accessible to all children and young people, through the creation of orchestras and choirs, as well as promoting respect for and recognition of people from disadvantaged and vulnerable groups of society. This musical lab will bring these ideals into the festival space, developing your musical knowledge and skills within our own comfortable and friendly AfroBanana community. Open to people of all musical abilities, this interactive workshop will include activating using percussion, movement, singing, music and instruments to promote creativity, empathy and a sense of security within the group.
 
-**Capacity:** No limits
+**Capacity:** 25 participants
 
 **Duration:** 1 hour
 
-**Age Group:**  All ages
+**Age Group:**  16+
 `.trim()
 
 export default {
