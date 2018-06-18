377ab4fc482b1369b28d084eb15638c0fa2dde77
marthabanana <marthag@afrobananarepublic.com>
2018-06-18 20:07:43 +0300

    Update JULIO.js

--- src/activities/fixtures/JULIO.js
+++ src/activities/fixtures/JULIO.js
@@ -7,7 +7,7 @@ export const name = 'RAP & BEATMAKING / PROCESS & IMPACT'
 export const text = `
 Ever wanted to know how a hip-hop track is created? Well, now is your chance! This exciting workshop will take aspiring young music makers and artists-in-training through the creative process from start to finish; teaching them how to mix and produce hip-hop, create unique lyrics that pack some serious punch and deliver an engaging rap performance. Island legends of the local hip-hop scene, JUΛIO and SMILIN will also share the scoop on their own unique genre of music, inspiring and engaging young minds with the importance of music and what it can achieve on a socio-political level.
 
-**Capacity:** No limits
+**Capacity:** 60 participants
 
 **Duration:** 2 hours
 
