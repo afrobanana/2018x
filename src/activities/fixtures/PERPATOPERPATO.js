bff94df1840450146e21290ea057a3ed54b184e2
marthabanana <marthag@afrobananarepublic.com>
2018-06-18 18:30:47 +0300

    Update PERPATOPERPATO.js

--- src/activities/fixtures/PERPATOPERPATO.js
+++ src/activities/fixtures/PERPATOPERPATO.js
@@ -5,7 +5,7 @@ export const photo = image
 // Markdown fields
 export const name = 'ΠΕΡΠΑΤΩ, ΠΕΡΠΑΤΩ ΣΤΟ ΔΑΣΟΣ'
 export const text = `
-The surreal magic of a festival is the perfect environment to lose yourself in nature’s enchanting mysticism. At this workshop, inquisitive festival goers of all ages will use all five senses to learn, discover and explore; listening to the fairy tales of the forest and uncovering its hidden secrets. What hides under the wings of a golden eagle? What does a giant, a princess, a boatman, a magician and an old lady have to smile about? And who is more cunning than a fox? Grab your parents and find out together!
+The surreal magic of a festival is the perfect environment to lose yourself in nature’s enchanting mysticism. At this performance, inquisitive festival goers of all ages will use all five senses to learn, discover and explore; listening to the fairy tales of the forest and uncovering its hidden secrets. What hides under the wings of a golden eagle? What does a giant, a princess, a boatman, a magician and an old lady have to smile about? And who is more cunning than a fox? Grab your parents and find out together!
 
 Narration: Elenitsa Georgiou (Music teacher, Narrator)
 Musical Accompaniment: Christina Polycarpou (Musician)
@@ -15,7 +15,7 @@ Musical Accompaniment: Christina Polycarpou (Musician)
 
 **Duration:** 1 hour
 
-**Age Group:**  All ages
+**Age Group:**  Children aged 5+
 `.trim()
 
 export default {
