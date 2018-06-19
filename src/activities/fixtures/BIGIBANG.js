7f7ab3785b7aa92eb0b2c06d2c6a9c47545565b3
marthabanana <marthag@afrobananarepublic.com>
2018-06-19 11:19:05 +0300

    Update BIGIBANG.js

--- src/activities/fixtures/BIGIBANG.js
+++ src/activities/fixtures/BIGIBANG.js
@@ -5,7 +5,7 @@ export const photo = image
 // Markdown fields
 export const name = 'BIGIBANG PROJECT'
 export const text = `
-If there is one thing that almost all kids are seriously good at, its making a bucket load of noise. And they’ll get to do just that at this riotously fun workshop aimed at boisterous little monsters with little or no knowledge of music. Drop your precious cargo off for an hour of experimenting with makeshift Junk Percussion instruments; learning new beats and rhythms; expressing themselves freely and creatively and generally letting off steam in a safe environment. You never know, you might have the world’s next John Bonham on your hands!
+If there is one thing that almost all kids are seriously good at, it's making a bucket load of noise. And they’ll get to do just that at this riotously fun workshop aimed at boisterous little monsters with little or no knowledge of music. Drop your precious cargo off for an hour of experimenting with makeshift Junk Percussion instruments; learning new beats and rhythms; expressing themselves freely and creatively and generally letting off steam in a safe environment. You never know, you might have the world’s next John Bonham on your hands!
 
 **Capacity:** 20 participants
 
