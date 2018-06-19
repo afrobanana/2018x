b7992cec5d4681b408d7b325fdf1037a219c0cbf
marthabanana <marthag@afrobananarepublic.com>
2018-06-19 11:25:21 +0300

    Update FTIAXEPARAMYTHI.js

--- src/activities/fixtures/FTIAXEPARAMYTHI.js
+++ src/activities/fixtures/FTIAXEPARAMYTHI.js
@@ -5,7 +5,7 @@ export const photo = image
 // Markdown fields
 export const name = 'ΦΤΙΑΞΕ ΠΑΡΑΜΥΘΙ'
 export const text = `
-Growing in a secret corner of Afrobanana Republic Festival this year, is The Majestic Music Tree; an enchanting place where the imagination is sparked, and fairy tales come to life. Music educator and storyteller Elenička Georgiou, and photographer Anca Clivet, have joined forces to co-create this interactive workshop, taking children on a journey of fantasy, deep into the world of magic, where they will create stories all of their own based on inspiring and expressive artistic photographs.
+Growing in a secret corner of Afrobanana Republic Festival this year, is The Majestic Music Tree; an enchanting place where the imagination is sparked, and fairy tales come to life. Music educator and storyteller Elenitsa Georgiou, and photographer Anca Clivet, have joined forces to co-create this interactive workshop, taking children on a journey of fantasy, deep into the world of magic, where they will create stories all of their own based on inspiring and expressive artistic photographs.
 
 **Capacity:** 20 Participants
 
